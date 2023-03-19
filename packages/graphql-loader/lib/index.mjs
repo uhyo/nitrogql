f1b0324242094b955473cc407917a9093d9fea46
uhyo <uhyo@uhy.ooo>
2023-03-19 23:34:56 +0900

    in a loader form, but...

--- packages/graphql-loader/lib/index.mjs
+++ packages/graphql-loader/lib/index.mjs
@@ -1,9 +1,10 @@
 #! /usr/bin/env node --experimental-wasi-unstable-preview1
 
-import { WASI } from 'node:wasi';
-import { argv, env, cwd } from 'node:process';
-import { readFile } from 'node:fs/promises';
-import { StringAllocator } from './alloc.mjs';
+import path from "node:path";
+import { WASI } from "node:wasi";
+import { argv, env, cwd } from "node:process";
+import { readFile } from "node:fs/promises";
+import { StringAllocator } from "./alloc.mjs";
 
 const CWD = cwd();
 
@@ -14,14 +15,14 @@ const wasi = new WASI({
     CWD,
   },
   preopens: {
-    [CWD]: CWD
+    [CWD]: CWD,
   },
 });
 
 const importObject = { wasi_snapshot_preview1: wasi.wasiImport };
 
 const wasm = await WebAssembly.compile(
-  await readFile(new URL('../wasm/graphql-loader.wasm', import.meta.url)),
+  await readFile(new URL("../wasm/graphql-loader.wasm", import.meta.url))
 );
 const instance = await WebAssembly.instantiate(wasm, importObject);
 
@@ -31,17 +32,34 @@ const alloc = new StringAllocator(instance);
 
 instance.exports.init();
 
-instance.exports.load_config(0, 0);
+let lastLoadedConfigPath = undefined;
 
-const inputString = alloc.allocString(`query { a b c }`);
+export default function graphQLLoader(source) {
+  const options = this.getOptions();
+  const configFile = options?.configFile;
+  if (lastLoadedConfigPath !== configFile && typeof configFile === "string") {
+    const configFilePath = path.resolve(this.rootContext, configFile);
+    const configFilePathString = alloc.allocString(configFilePath);
+    instance.exports.load_config(
+      configFilePathString.ptr,
+      configFilePathString.size
+    );
+    configFilePathString.free();
+  }
 
-const convertResult = instance.exports.convert_to_js(inputString.ptr, inputString.size);
+  const inputString = alloc.allocString(source);
 
-inputString.free();
-
-if (convertResult) {
-  const ptr = instance.exports.get_result_ptr();
-  const size = instance.exports.get_result_size();
-  const result = alloc.readString(ptr, size);
-  console.log({ result })
+  const convertResult = instance.exports.convert_to_js(
+    inputString.ptr,
+    inputString.size
+  );
+  inputString.free();
+  if (convertResult) {
+    const ptr = instance.exports.get_result_ptr();
+    const size = instance.exports.get_result_size();
+    const result = alloc.readString(ptr, size);
+    return result;
+  } else {
+    throw new Error("graphql-loader failed to convert");
+  }
 }
