5ec21db2d3519da30e87cc0c315bd20657a3cdd6
uhyo <uhyo@uhy.ooo>
2023-04-02 21:47:20 +0900

    enable color on TTY

--- packages/cli/bin/index.mjs
+++ packages/cli/bin/index.mjs
@@ -1,15 +1,20 @@
-#! /usr/bin/env node --experimental-wasi-unstable-preview1
+#! /usr/bin/env node --no-warnings --experimental-wasi-unstable-preview1
 
 import { WASI } from "node:wasi";
-import { argv, env, cwd } from "node:process";
+import { argv, env, cwd, stdout } from "node:process";
 import { readFile } from "node:fs/promises";
 import { shim } from "./shim.mjs";
 
 const CWD = cwd();
+const isTTY = stdout.isTTY;
 
 const wasi = new WASI({
   args: argv.slice(1),
   env: {
+    // env_logger
+    RUST_LOG_STYLE: isTTY ? "always" : "auto",
+    // colored
+    CLICOLOR_FORCE: isTTY ? "1" : "0",
     ...env,
     CWD,
   },
