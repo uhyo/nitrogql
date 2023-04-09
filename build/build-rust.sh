74c7885bd2a931c925955559ebc90383bdafaedd
uhyo <uhyo@uhy.ooo>
2023-04-09 17:25:51 +0900

    integrate wasm-snip to build flow

--- build/build-rust.sh
+++ build/build-rust.sh
@@ -2,14 +2,15 @@
 set -eux
 
 # Script to build rust code.
-# Requirements: cargo, wasm-opt (from binaryen)
+# Requirements: cargo, wasm-opt (from binaryen), wasm-snip (from rustwasm)
 (
   cd crates/cli
   cargo rustc --target wasm32-wasi --release
 )
 (
   cd target/wasm32-wasi/release
-  wasm-opt nitrogql-cli.wasm -Oz -o nitrogql-cli.opt.wasm
+  wasm-opt nitrogql-cli.wasm -Oz -o nitrogql-cli.opt.wasm.tmp
+  wasm-snip nitrogql-cli.opt.wasm.tmp > nitrogql-cli.opt.wasm
 )
 
 (
@@ -18,5 +19,6 @@ set -eux
 )
 (
   cd target/wasm32-unknown-unknown/release
-  wasm-opt graphql-loader.wasm -Oz -o graphql-loader.opt.wasm
+  wasm-opt graphql-loader.wasm -Oz -o graphql-loader.opt.wasm.tmp
+  wasm-snip graphql-loader.opt.wasm.tmp > graphql-loader.opt.wasm
 )
