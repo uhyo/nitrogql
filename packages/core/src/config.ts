1794cc2a3e044f26c098b4524e2bba037fb6d9de
uhyo <uhyo@uhy.ooo>
2023-04-26 21:23:22 +0900

    config js loading feature

---
+++ packages/core/src/config.ts
@@ -0,0 +1,97 @@
+/**
+ * @file `nitrogql_helper/config` namespace.
+ */
+
+import { execFileSync } from "node:child_process";
+import { getMemory, readString } from "./memory.js";
+
+export type NitrogqlConfigNamespace = {
+  /**
+   * Executes given config file.
+   * Returns the handle to the result.
+   * 0 if there was an error.
+   */
+  execute_config_file(
+    config_file_path: number,
+    config_file_path_len: number
+  ): number;
+  /**
+   * Returns the length of the result.
+   */
+  result_len(handle: number): number;
+  /**
+   * Writes result of executing config file to given buffer.
+   * Returns the number of bytes written.
+   */
+  write_result_to_buffer(
+    handle: number,
+    buffer: number,
+    buffer_len: number
+  ): number;
+  /**
+   * Frees the result.
+   */
+  free_result(handle: number): void;
+};
+
+export const config: NitrogqlConfigNamespace = {
+  execute_config_file,
+  result_len,
+  write_result_to_buffer,
+  free_result,
+};
+
+const handleMap = new Map<number, string>();
+let handleCounter = 0;
+
+function execute_config_file(
+  config_file_path: number,
+  config_file_path_len: number
+): number {
+  const configFilePath = readString(config_file_path, config_file_path_len);
+  try {
+    const result = execFileSync("node", ["--input-type=module"], {
+      encoding: "utf-8",
+      input: `
+import config from ${JSON.stringify(configFilePath)};
+import { stdout } from "process";
+stdout.write(JSON.stringify(config));
+`,
+    });
+    const handle = ++handleCounter;
+    handleMap.set(handle, result);
+    return handle;
+  } catch (err) {
+    console.error(err);
+    return 0;
+  }
+}
+
+function result_len(handle: number): number {
+  const result = handleMap.get(handle);
+  if (result === undefined) {
+    return 0;
+  }
+  return result.length;
+}
+
+function write_result_to_buffer(
+  handle: number,
+  buffer: number,
+  buffer_len: number
+): number {
+  const result = handleMap.get(handle);
+  if (result === undefined) {
+    return 0;
+  }
+  const bytes = new TextEncoder().encode(result);
+  const memory = getMemory();
+  const bytesToWrite = Math.min(bytes.length, buffer_len);
+  const bufferView = new Uint8Array(memory.buffer, buffer, bytesToWrite);
+  bufferView.set(bytes.slice(0, bytesToWrite));
+  return bytesToWrite;
+}
+
+function free_result(handle: number): void {
+  handleMap.delete(handle);
+}
---
+++ packages/core/src/index.ts
@@ -0,0 +1,13 @@
+import { config } from "./config.js";
+import { setMemory } from "./memory.js";
+
+export {
+  /**
+   * Sets the memory used by the wasm module.
+   */
+  setMemory,
+  /**
+   * `nitrogql_helper/config` namespace
+   */
+  config,
+};
