9f9938cfecef9259aff3866180c861bef3750ca2
uhyo <uhyo@uhy.ooo>
2023-04-29 22:44:38 +0900

    chore: v1.0.0-beta.2

--- build/build-node.sh
+++ build/build-node.sh
@@ -5,6 +5,8 @@ set -eux
 # Expected to be run after `build-rust.sh`
 # Requirements: node
 
+npm run build -w @nitrogql/core
+
 mkdir -p packages/cli/wasm
 cp target/wasm32-wasi/release/nitrogql-cli.opt.wasm packages/cli/wasm/nitrogql-cli.wasm
 
@@ -16,4 +18,5 @@ cp target/wasm32-unknown-unknown/release/graphql-loader.opt.wasm packages/rollup
 
 current_version=$(npm pkg get version --json)
 npm pkg set version=${current_version} --json --workspaces
+npm pkg set dependencies.@nitrogql/core=${current_version} --json -w @nitrogql/cli
 npx prettier --write "./**/package.json"
