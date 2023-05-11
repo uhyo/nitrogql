f4118493e80aab3aaf7f862cf4786d6f69be1fe4
uhyo <uhyo@uhy.ooo>
2023-05-11 20:58:53 +0900

    feat: connect option

--- crates/cli/src/generate.rs
+++ crates/cli/src/generate.rs
@@ -64,7 +64,10 @@ pub fn run_generate(mut context: CliContext) -> Result<CliContext> {
                         .collect(),
                 };
 
-                let mut options = SchemaTypePrinterOptions::default();
+                let mut options = SchemaTypePrinterOptions {
+                    emit_schema_runtime: config.config.generate.emit_schema_runtime,
+                    ..SchemaTypePrinterOptions::default()
+                };
                 options.scalar_types.extend(
                     config
                         .config
@@ -73,6 +76,7 @@ pub fn run_generate(mut context: CliContext) -> Result<CliContext> {
                         .iter()
                         .map(|(key, value)| (key.to_owned(), value.to_owned())),
                 );
+
                 let mut writer = SourceWriter::new();
                 writer.set_file_index_mapper(file_map.file_indices.clone());
                 let mut printer = SchemaTypePrinter::new(options, &mut writer);
