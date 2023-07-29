97e1c7f76329fa809e5f9dfd97f0cb9aff19430f
uhyo <uhyo@uhy.ooo>
2023-07-29 18:36:59 +0900

    feat(website): install toc to all pages

--- website/src/app/(docs)/cli/page.tsx
+++ website/src/app/(docs)/cli/page.tsx
@@ -1,90 +1,94 @@
 import { Hint } from "@/app/_utils/Hint";
 import { Highlight } from "@/app/_utils/Highlight";
+import { Toc } from "../_toc";
 
 export default function GettingStarted() {
   return (
-    <main>
-      <h2>nitrogql CLI Usage</h2>
-      <p>
-        nitrogql provides a CLI to check your GraphQL code and to generate types
-        from your schema and operations.
-      </p>
-      <p>
-        The CLI is installed with <code>npm</code>:
-      </p>
-      <Highlight language="bash">{`npm install --save-dev @nitrogql/cli`}</Highlight>
+    <Toc>
+      <main>
+        <h2>nitrogql CLI Usage</h2>
+        <p>
+          nitrogql provides a CLI to check your GraphQL code and to generate
+          types from your schema and operations.
+        </p>
+        <p>
+          The CLI is installed with <code>npm</code>:
+        </p>
+        <Highlight language="bash">{`npm install --save-dev @nitrogql/cli`}</Highlight>
 
-      <h3>commands</h3>
-      <p>Basic usage of the CLI is:</p>
-      <Highlight language="bash">{`nitrogql <command> [options]`}</Highlight>
-      <p>The following commands are available.</p>
-      <ul>
-        <li>
-          <code>check</code>: Check your GraphQL code.
-        </li>
-        <li>
-          <code>generate</code>: Generate types from your schema and operations.
-        </li>
-      </ul>
-      <Hint>
-        💡 <code>generate</code> also implies <code>check</code>. GraphQL code
-        is checked before types are generated.
-      </Hint>
+        <h3 id="commands">commands</h3>
+        <p>Basic usage of the CLI is:</p>
+        <Highlight language="bash">{`nitrogql <command> [options]`}</Highlight>
+        <p>The following commands are available.</p>
+        <ul>
+          <li>
+            <code>check</code>: Check your GraphQL code.
+          </li>
+          <li>
+            <code>generate</code>: Generate types from your schema and
+            operations.
+          </li>
+        </ul>
+        <Hint>
+          💡 <code>generate</code> also implies <code>check</code>. GraphQL code
+          is checked before types are generated.
+        </Hint>
 
-      <h3>options</h3>
+        <h3 id="options">options</h3>
 
-      <h4>
-        <code>--config-file</code>
-      </h4>
-      <p>
-        Specify the path to the configuration file. By default, the CLI looks
-        for <code>graphql.config.yaml</code> in the current directory.
-      </p>
-      <Highlight language="bash">{`npx nitrogql generate --config-file ./path/to/config.yaml`}</Highlight>
+        <h4 id="config-file">
+          <code>--config-file</code>
+        </h4>
+        <p>
+          Specify the path to the configuration file. By default, the CLI looks
+          for <code>graphql.config.yaml</code> in the current directory.
+        </p>
+        <Highlight language="bash">{`npx nitrogql generate --config-file ./path/to/config.yaml`}</Highlight>
 
-      <h4>
-        <code>--schema</code>
-      </h4>
-      <p>
-        Specify the path to the schema file(s). This overrides the schema path
-        specified in the configuration file.
-      </p>
+        <h4 id="schema">
+          <code>--schema</code>
+        </h4>
+        <p>
+          Specify the path to the schema file(s). This overrides the schema path
+          specified in the configuration file.
+        </p>
 
-      <h4>
-        <code>--operation</code>
-      </h4>
-      <p>
-        Specify the path to the operation file(s). This overrides the operation
-        path specified in the configuration file.
-      </p>
+        <h4 id="operation">
+          <code>--operation</code>
+        </h4>
+        <p>
+          Specify the path to the operation file(s). This overrides the
+          operation path specified in the configuration file.
+        </p>
 
-      <h4>
-        <code>--output-format</code>
-      </h4>
-      <p>Specify the output format of the CLI. Possible values are:</p>
-      <ul>
-        <li>
-          <code>human</code> (default): prints human-readable output to stderr.
-        </li>
-        <li>
-          <code>rdjson</code>: prints &apos;check&apos; results in{" "}
-          <a
-            href="https://github.com/reviewdog/reviewdog/tree/master/proto/rdf"
-            target="_blank"
-          >
-            rdjson
-          </a>{" "}
-          format to stdout. This is useful for integrating with reviewdog. Makes
-          sense only when the <code>check</code> command is run.
-        </li>
-        <li>
-          <code>json</code>: prints nitrogql specific JSON output to stdout.
-        </li>
-      </ul>
-      <p>
-        The signature of the <code>json</code> output is:
-      </p>
-      <Highlight language="typescript">{`interface CLIOutput {
+        <h4 id="output-format">
+          <code>--output-format</code>
+        </h4>
+        <p>Specify the output format of the CLI. Possible values are:</p>
+        <ul>
+          <li>
+            <code>human</code> (default): prints human-readable output to
+            stderr.
+          </li>
+          <li>
+            <code>rdjson</code>: prints &apos;check&apos; results in{" "}
+            <a
+              href="https://github.com/reviewdog/reviewdog/tree/master/proto/rdf"
+              target="_blank"
+            >
+              rdjson
+            </a>{" "}
+            format to stdout. This is useful for integrating with reviewdog.
+            Makes sense only when the <code>check</code> command is run.
+          </li>
+          <li>
+            <code>json</code>: prints nitrogql specific JSON output to stdout.
+          </li>
+        </ul>
+        <p>
+          The signature of the <code>json</code> output is:
+        </p>
+        <Highlight language="typescript">{`interface CLIOutput {
   /**
    * Exists when a command fails.
    */
@@ -135,14 +139,15 @@ export default function GettingStarted() {
   }
 }`}</Highlight>
 
-      <h3>Notes on file system access</h3>
-      <p>
-        Due to the security nature of WASI, the CLI cannot access files outside
-        the current directory by default. To allow access to files outside the
-        current directory, use a <code>NITROGQL_FS_SCOPE</code> environment
-        variable.
-      </p>
-      <Highlight language="bash">{`NITROGQL_FS_SCOPE=../ npx nitrogql check --config-file ../graphql.config.yaml`}</Highlight>
-    </main>
+        <h3 id="notes-on-file-system-access">Notes on file system access</h3>
+        <p>
+          Due to the security nature of WASI, the CLI cannot access files
+          outside the current directory by default. To allow access to files
+          outside the current directory, use a <code>NITROGQL_FS_SCOPE</code>{" "}
+          environment variable.
+        </p>
+        <Highlight language="bash">{`NITROGQL_FS_SCOPE=../ npx nitrogql check --config-file ../graphql.config.yaml`}</Highlight>
+      </main>
+    </Toc>
   );
 }
--- website/src/app/(docs)/getting-started/page.tsx
+++ website/src/app/(docs)/getting-started/page.tsx
@@ -4,73 +4,78 @@ import { Hint } from "@/app/_utils/Hint";
 import { Highlight } from "@/app/_utils/Highlight";
 import { Figures } from "@/app/_utils/Figures";
 import ScreenshotGeneratedTypes from "./figures/screenshot-generated-types.png";
+import { Toc } from "../_toc";
 
 export default function GettingStarted() {
   return (
-    <main>
-      <h2>Getting Started</h2>
-      <p>
-        This page guides you through the steps to get started with nitrogql.
-      </p>
+    <Toc>
+      <main>
+        <h2>Getting Started</h2>
+        <p>
+          This page guides you through the steps to get started with nitrogql.
+        </p>
 
-      <h3 id="installation">Installation</h3>
-      <p>
-        The nitrogql CLI can be installed with npm. The CLI is required for any
-        workflow using nitrogql. Since generated types depend on{" "}
-        <a
-          href="https://github.com/dotansimha/graphql-typed-document-node"
-          target="_blank"
-        >
-          TypedDocumentNode
-        </a>
-        , you also need to install it to your project.
-      </p>
-      <Highlight language="bash">{`npm install --save-dev @nitrogql/cli @graphql-typed-document-node/core`}</Highlight>
-      <p>
-        Depending on framework or tool you use, you may also need to install
-        appropriate loader packages.
-      </p>
-      <p>
-        For <b>webpack-based tools</b>:
-      </p>
-      <Highlight language="bash">{`npm install --save-dev @nitrogql/graphql-loader`}</Highlight>
-      <p>
-        For <b>Rollup-based tools</b>:
-      </p>
-      <Highlight language="bash">{`npm install --save-dev @nitrogql/rollup-plugin`}</Highlight>
+        <h3 id="installation">Installation</h3>
+        <p>
+          The nitrogql CLI can be installed with npm. The CLI is required for
+          any workflow using nitrogql. Since generated types depend on{" "}
+          <a
+            href="https://github.com/dotansimha/graphql-typed-document-node"
+            target="_blank"
+          >
+            TypedDocumentNode
+          </a>
+          , you also need to install it to your project.
+        </p>
+        <Highlight language="bash">{`npm install --save-dev @nitrogql/cli @graphql-typed-document-node/core`}</Highlight>
+        <p>
+          Depending on framework or tool you use, you may also need to install
+          appropriate loader packages.
+        </p>
+        <p>
+          For <b>webpack-based tools</b>:
+        </p>
+        <Highlight language="bash">{`npm install --save-dev @nitrogql/graphql-loader`}</Highlight>
+        <p>
+          For <b>Rollup-based tools</b>:
+        </p>
+        <Highlight language="bash">{`npm install --save-dev @nitrogql/rollup-plugin`}</Highlight>
 
-      <h3 id="configuration-file">Configuration File</h3>
-      <p>
-        nitrogql uses a configuration file to specify the location of your
-        schema and operations. The configuration file is named{" "}
-        <code>graphql.config.yaml</code>. The configuration file should be
-        placed in the root of your project.
-      </p>
-      <p>The simplest configuration file will look like:</p>
-      <Highlight language="yaml">
-        {`schema: ./schema/*.graphql
+        <h3 id="configuration-file">Configuration File</h3>
+        <p>
+          nitrogql uses a configuration file to specify the location of your
+          schema and operations. The configuration file is named{" "}
+          <code>graphql.config.yaml</code>. The configuration file should be
+          placed in the root of your project.
+        </p>
+        <p>The simplest configuration file will look like:</p>
+        <Highlight language="yaml">
+          {`schema: ./schema/*.graphql
 documents: ./app/**/*.graphql`}
-      </Highlight>
-      <p>
-        This file follows{" "}
-        <a href="https://the-guild.dev/graphql/config/docs" target="_blank">
-          the GraphQL Config convention
-        </a>{" "}
-        from The Guild. You might already have a configuration file if you use
-        other GraphQL tools.
-      </p>
-      <p>
-        See <Link href="/configuration">Configuration</Link> for full details
-        about the configuration file.
-      </p>
+        </Highlight>
+        <p>
+          This file follows{" "}
+          <a href="https://the-guild.dev/graphql/config/docs" target="_blank">
+            the GraphQL Config convention
+          </a>{" "}
+          from The Guild. You might already have a configuration file if you use
+          other GraphQL tools.
+        </p>
+        <p>
+          See <Link href="/configuration">Configuration</Link> for full details
+          about the configuration file.
+        </p>
 
-      <h3>Setting up GraphQL loader for webpack</h3>
-      <p>
-        If you are using webpack-based tools, you need to configure the loader.
-      </p>
-      <p>Example setup (webpack.config.js):</p>
-      <Highlight language="javascript">
-        {`module: {
+        <h3 id="setting-up-graphql-loader-for-webpack">
+          Setting up GraphQL loader for webpack
+        </h3>
+        <p>
+          If you are using webpack-based tools, you need to configure the
+          loader.
+        </p>
+        <p>Example setup (webpack.config.js):</p>
+        <Highlight language="javascript">
+          {`module: {
   rules: [
     // ...
     {
@@ -82,24 +87,26 @@ documents: ./app/**/*.graphql`}
     }
   ]
 }`}
-      </Highlight>
-      <Hint>
-        🔥 We have a Next.js example:{" "}
-        <a
-          href="https://github.com/uhyo/nitrogql/tree/master/examples/nextjs"
-          target="_blank"
-        >
-          see on GitHub
-        </a>
-      </Hint>
+        </Highlight>
+        <Hint>
+          🔥 We have a Next.js example:{" "}
+          <a
+            href="https://github.com/uhyo/nitrogql/tree/master/examples/nextjs"
+            target="_blank"
+          >
+            see on GitHub
+          </a>
+        </Hint>
 
-      <h3>Setting up GraphQL loader for Rollup</h3>
-      <p>
-        If you are using Rollup-based tools, you need to configure the plugin.
-      </p>
-      <p>Example setup (rollup.config.js):</p>
-      <Highlight language="javascript">
-        {`import nitrogql from "@nitrogql/rollup-plugin";
+        <h3 id="setting-up-graphql-loader-for-rollup">
+          Setting up GraphQL loader for Rollup
+        </h3>
+        <p>
+          If you are using Rollup-based tools, you need to configure the plugin.
+        </p>
+        <p>Example setup (rollup.config.js):</p>
+        <Highlight language="javascript">
+          {`import nitrogql from "@nitrogql/rollup-plugin";
 
 export default {
   plugins: [
@@ -109,125 +116,128 @@ export default {
   ],
 };
 `}
-      </Highlight>
-      <Hint>
-        🔥 We have a Vite example:{" "}
-        <a
-          href="https://github.com/uhyo/nitrogql/tree/master/examples/vite"
-          target="_blank"
-        >
-          see on GitHub
-        </a>
-      </Hint>
+        </Highlight>
+        <Hint>
+          🔥 We have a Vite example:{" "}
+          <a
+            href="https://github.com/uhyo/nitrogql/tree/master/examples/vite"
+            target="_blank"
+          >
+            see on GitHub
+          </a>
+        </Hint>
 
-      <h3>Statically checking GraphQL files</h3>
-      <p>
-        In the following explanations, we will assume that GraphQL schemas and
-        operations are already prepared. If you are new to GraphQL, it is a good
-        idea to check out{" "}
-        <a
-          href="https://github.com/uhyo/nitrogql/tree/master/examples/vite"
-          target="_blank"
-        >
-          our examples
-        </a>{" "}
-        and proceed.
-      </p>
-      <p>
-        To check GraphQL files, run the following command in your project
-        directory:
-      </p>
-      <Highlight language="bash">{`npx nitrogql check`}</Highlight>
-      <Hint>
-        💡 <b>Note</b>: nitrogql CLI looks for the configuration file{" "}
-        <code>graphql.config.yaml</code> in the current directory by default. To
-        specify the location of the configuration file, use the{" "}
-        <code>--config-file</code> option.
-      </Hint>
-      <p>
-        If all GraphQL files are valid, the command will exit with code 0.
-        Otherwise, it will print errors and exit with code 1.
-      </p>
+        <h3 id="statically-checking-graphql-files">
+          Statically checking GraphQL files
+        </h3>
+        <p>
+          In the following explanations, we will assume that GraphQL schemas and
+          operations are already prepared. If you are new to GraphQL, it is a
+          good idea to check out{" "}
+          <a
+            href="https://github.com/uhyo/nitrogql/tree/master/examples/vite"
+            target="_blank"
+          >
+            our examples
+          </a>{" "}
+          and proceed.
+        </p>
+        <p>
+          To check GraphQL files, run the following command in your project
+          directory:
+        </p>
+        <Highlight language="bash">{`npx nitrogql check`}</Highlight>
+        <Hint>
+          💡 <b>Note</b>: nitrogql CLI looks for the configuration file{" "}
+          <code>graphql.config.yaml</code> in the current directory by default.
+          To specify the location of the configuration file, use the{" "}
+          <code>--config-file</code> option.
+        </Hint>
+        <p>
+          If all GraphQL files are valid, the command will exit with code 0.
+          Otherwise, it will print errors and exit with code 1.
+        </p>
 
-      <h3>Generating TypeScript types</h3>
-      <p>
-        nitrogql generates one <code>.d.ts</code> file for schema, and one{" "}
-        <code>.d.graphql.ts</code>
-        for each operation file. <code>.d.graphql.ts</code> files are placed
-        next to operation files.
-      </p>
-      <Hint>
-        💡 <b>Note</b>: <code>.d.graphql.ts</code> files are supported by
-        TypeScript 5.0 or later. If you are using TypeScript 4.x, you need to
-        configure nitrogql to generate <code>.graphql.d.ts</code> files instead.
-        See <Link href="/configuration">Configuration</Link> for details.
-      </Hint>
-      <p>
-        Before generating types, you need to specify the location of generated
-        schema file in the configuration file:
-      </p>
-      <Highlight language="yaml">
-        {`schema: ./schema/*.graphql
+        <h3 id="generating-typescript-types">Generating TypeScript types</h3>
+        <p>
+          nitrogql generates one <code>.d.ts</code> file for schema, and one{" "}
+          <code>.d.graphql.ts</code>
+          for each operation file. <code>.d.graphql.ts</code> files are placed
+          next to operation files.
+        </p>
+        <Hint>
+          💡 <b>Note</b>: <code>.d.graphql.ts</code> files are supported by
+          TypeScript 5.0 or later. If you are using TypeScript 4.x, you need to
+          configure nitrogql to generate <code>.graphql.d.ts</code> files
+          instead. See <Link href="/configuration">Configuration</Link> for
+          details.
+        </Hint>
+        <p>
+          Before generating types, you need to specify the location of generated
+          schema file in the configuration file:
+        </p>
+        <Highlight language="yaml">
+          {`schema: ./schema/*.graphql
 documents:
 extensions:
   nitrogql:
     generate:
       schemaOutput: ./src/generated/schema.d.ts`}
-      </Highlight>
-      <p>
-        There is no option to specify the location of generated types for
-        operation files; they are always placed next to operation files.
-      </p>
-      <p>
-        To generate TypeScript types, run the following command in your project
-        directory:
-      </p>
-      <Highlight language="bash">{`npx nitrogql generate`}</Highlight>
-      <Hint>
-        💡 <b>Note</b>: the <code>generate</code> command implies{" "}
-        <code>check</code>. If there are errors in GraphQL files, the command
-        fails and does not generate any files.
-      </Hint>
-      <p>
-        After a successful run, you will see types generated next to your
-        operation files.
-      </p>
-      <Figures>
-        <figure>
-          <Image
-            src={ScreenshotGeneratedTypes}
-            width="640"
-            alt="Screenshot of VSCode showing generated types"
-          />
-          <figcaption>
-            Type definitions and source maps are generated next to each
-            operation.
-          </figcaption>
-        </figure>
-      </Figures>
-      <p>
-        Now you can import <code>.graphql</code> files in your TypeScript code,
-        but probably you need to adjust your <code>tsconfig.json</code> so that
-        TypeScript allows importing <code>.graphql</code> files.
-      </p>
-      <Highlight language="diff">
-        {` {
+        </Highlight>
+        <p>
+          There is no option to specify the location of generated types for
+          operation files; they are always placed next to operation files.
+        </p>
+        <p>
+          To generate TypeScript types, run the following command in your
+          project directory:
+        </p>
+        <Highlight language="bash">{`npx nitrogql generate`}</Highlight>
+        <Hint>
+          💡 <b>Note</b>: the <code>generate</code> command implies{" "}
+          <code>check</code>. If there are errors in GraphQL files, the command
+          fails and does not generate any files.
+        </Hint>
+        <p>
+          After a successful run, you will see types generated next to your
+          operation files.
+        </p>
+        <Figures>
+          <figure>
+            <Image
+              src={ScreenshotGeneratedTypes}
+              width="640"
+              alt="Screenshot of VSCode showing generated types"
+            />
+            <figcaption>
+              Type definitions and source maps are generated next to each
+              operation.
+            </figcaption>
+          </figure>
+        </Figures>
+        <p>
+          Now you can import <code>.graphql</code> files in your TypeScript
+          code, but probably you need to adjust your <code>tsconfig.json</code>{" "}
+          so that TypeScript allows importing <code>.graphql</code> files.
+        </p>
+        <Highlight language="diff">
+          {` {
    "compilerOptions": {
      // ...
 +    "allowArbitraryExtensions": true,
      // ...
    }
  }`}
-      </Highlight>
-      <p>
-        After configuring TypeScript correctly, it&apos;s time to import{" "}
-        <code>.graphql</code> files in your code. These files export{" "}
-        <code>DocumentNode</code> objects as default exports. You can use them
-        with any GraphQL client library. Below are examples for Apollo Client
-        and React:
-      </p>
-      <Highlight language="typescript">
-        {`import { useQuery } from "@apollo/client";
+        </Highlight>
+        <p>
+          After configuring TypeScript correctly, it&apos;s time to import{" "}
+          <code>.graphql</code> files in your code. These files export{" "}
+          <code>DocumentNode</code> objects as default exports. You can use them
+          with any GraphQL client library. Below are examples for Apollo Client
+          and React:
+        </p>
+        <Highlight language="typescript">
+          {`import { useQuery } from "@apollo/client";
 import SampleQuery from "./SampleQuery.graphql";
 
 export function SampleComponent() {
@@ -235,15 +245,16 @@ export function SampleComponent() {
 
   return <div>{data?.sample}</div>;
 }`}
-      </Highlight>
-      <p>
-        Of course this is type-safe. If your code accesses a field that does not
-        exist in the schema, TypeScript will report an error.
-      </p>
-      <Hint>
-        🧺 <b>Read Next</b>: <Link href="/configuration">Configuration</Link>,{" "}
-        <Link href="/cli">CLI Usage</Link>
-      </Hint>
-    </main>
+        </Highlight>
+        <p>
+          Of course this is type-safe. If your code accesses a field that does
+          not exist in the schema, TypeScript will report an error.
+        </p>
+        <Hint>
+          🧺 <b>Read Next</b>: <Link href="/configuration">Configuration</Link>,{" "}
+          <Link href="/cli">CLI Usage</Link>
+        </Hint>
+      </main>
+    </Toc>
   );
 }
