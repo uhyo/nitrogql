907b0324aafdcdea94213a67cb0e8c0043ba5082
uhyo <uhyo@uhy.ooo>
2023-08-22 22:05:25 +0900

    build: add JS and E2E test

--- build/build-rust.sh
+++ build/build-rust.sh
@@ -2,23 +2,28 @@
 set -eux
 
 # Script to build rust code.
-# Requirements: cargo, wasm-opt (from binaryen), wasm-snip (from rustwasm)
+# Requirements: cargo.
+# If OPTIMIZE is set, additionally wasm-opt (from binaryen), wasm-snip (from rustwasm)
 (
   cd crates/cli
   cargo rustc --target wasm32-wasi --release
-)
-(
-  cd target/wasm32-wasi/release
-  wasm-opt nitrogql-cli.wasm -Oz -o nitrogql-cli.opt.wasm.tmp
-  wasm-snip nitrogql-cli.opt.wasm.tmp > nitrogql-cli.opt.wasm
 )
 
 (
   cd crates/graphql-loader
   cargo rustc --target wasm32-unknown-unknown --release
 )
-(
-  cd target/wasm32-unknown-unknown/release
-  wasm-opt graphql-loader.wasm -Oz -o graphql-loader.opt.wasm.tmp
-  wasm-snip graphql-loader.opt.wasm.tmp > graphql-loader.opt.wasm
-)
+
+if [ -n "${OPTIMIZE+x}" ]; then
+  (
+    cd target/wasm32-wasi/release
+    wasm-opt nitrogql-cli.wasm -Oz -o nitrogql-cli.opt.wasm.tmp
+    wasm-snip nitrogql-cli.opt.wasm.tmp > nitrogql-cli.opt.wasm
+  )
+
+  (
+    cd target/wasm32-unknown-unknown/release
+    wasm-opt graphql-loader.wasm -Oz -o graphql-loader.opt.wasm.tmp
+    wasm-snip graphql-loader.opt.wasm.tmp > graphql-loader.opt.wasm
+  )
+fi
