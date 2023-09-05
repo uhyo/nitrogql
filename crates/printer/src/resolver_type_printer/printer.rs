4d70415da849cc1390a780cd7a1aa7111401d25a
uhyo <uhyo@uhy.ooo>
2023-09-05 22:04:47 +0900

    basic object resolver

--- crates/printer/src/resolver_type_printer/printer.rs
+++ crates/printer/src/resolver_type_printer/printer.rs
@@ -1,5 +1,10 @@
-use nitrogql_ast::TypeSystemDocument;
+use nitrogql_ast::{type_system::TypeSystemDefinition, TypeSystemDocument};
 use sourcemap_writer::SourceMapWriter;
+
+use crate::{
+    resolver_type_printer::visitor::get_resolver_type,
+    ts_types::{ObjectKey, TSType},
+};
 
 use super::{
     error::ResolverTypePrinterResult, options::ResolverTypePrinterOptions, visitor::TypePrinter,
@@ -32,19 +37,44 @@ where
             document,
         };
 
-        write!(
+        writeln!(
             self.writer,
-            "import type * as {} from \"{}\";\n\n",
+            "import type {{ GraphQLResolveInfo }} from \"graphql\";"
+        );
+        writeln!(
+            self.writer,
+            "import type * as {} from \"{}\";",
             context.options.schema_root_namespace, context.options.schema_source,
+        );
+        writeln!(
+            self.writer,
+            "type __Resolver<Parent, Args, Context, Result> = (parent: Parent, args: Args, context: Context, info: GraphQLResolveInfo) => Result | Promise<Result>;"
         );
 
         for type_definition in &document.definitions {
             type_definition.print_type(&context, self.writer)?;
         }
 
-        self.writer.write("export type ");
-        self.writer.write(&context.options.root_resolver_type);
-        self.writer.write(" = {};");
+        let root_resolvers_type =
+            TSType::object(document.definitions.iter().filter_map(|type_definition| {
+                match type_definition {
+                    TypeSystemDefinition::TypeDefinition(type_definition) => {
+                        let resolver_type = get_resolver_type(type_definition, &context)?;
+                        Some((ObjectKey::from(type_definition.name()), resolver_type, None))
+                    }
+                    _ => None,
+                }
+            }));
+
+        write!(
+            self.writer,
+            "export type {}<Context> = ",
+            &context.options.root_resolver_type
+        );
+
+        root_resolvers_type.print_type(self.writer);
+        writeln!(self.writer, ";");
+
         Ok(())
     }
 }
--- crates/printer/src/resolver_type_printer/visitor.rs
+++ crates/printer/src/resolver_type_printer/visitor.rs
@@ -1,8 +1,9 @@
-use nitrogql_ast::{
-    type_system::{ScalarTypeDefinition, TypeDefinition, TypeSystemDefinition},
-    TypeSystemDocument,
+use nitrogql_ast::type_system::{
+    ArgumentsDefinition, ScalarTypeDefinition, TypeDefinition, TypeSystemDefinition,
 };
 use sourcemap_writer::SourceMapWriter;
+
+use crate::ts_types::{type_to_ts_type::get_ts_type_of_type, ObjectField, ObjectKey, TSType};
 
 use super::{error::ResolverTypePrinterResult, printer::ResolverTypePrinterContext};
 
@@ -61,4 +62,74 @@ impl TypePrinter for ScalarTypeDefinition<'_> {
 
         Ok(())
     }
+}
+
+pub fn get_resolver_type(
+    def: &TypeDefinition<'_>,
+    context: &ResolverTypePrinterContext,
+) -> Option<TSType> {
+    match def {
+        TypeDefinition::Scalar(_) => None,
+        TypeDefinition::Object(def) => get_object_resolver_type(def, context),
+        TypeDefinition::Interface(_) => None,
+        TypeDefinition::Union(_) => None,
+        TypeDefinition::Enum(_) => None,
+        TypeDefinition::InputObject(_) => None,
+    }
+}
+
+fn get_object_resolver_type(
+    def: &nitrogql_ast::type_system::ObjectTypeDefinition<'_>,
+    context: &ResolverTypePrinterContext,
+) -> Option<TSType> {
+    let parent_type = TSType::TypeVariable((&def.name).into());
+    let fields = def
+        .fields
+        .iter()
+        .map(|field| {
+            let arguments_type = field
+                .arguments
+                .as_ref()
+                .map_or_else(|| TSType::Object(vec![]), arguments_definition_to_ts);
+            let result_type = get_ts_type_of_type(&field.r#type, |name| {
+                TSType::TypeVariable((&name.name).into())
+            });
+
+            let resolver_type = TSType::TypeFunc(
+                Box::new(TSType::TypeVariable("__Resolver".into())),
+                vec![
+                    // Parent
+                    parent_type.clone(),
+                    // Args
+                    arguments_type,
+                    // Context
+                    TSType::TypeVariable("Context".into()),
+                    // Result
+                    result_type,
+                ],
+            );
+
+            ObjectField {
+                key: ObjectKey::from(&field.name),
+                r#type: resolver_type,
+                optional: false,
+                readonly: false,
+                description: None,
+            }
+        })
+        .collect();
+    Some(TSType::Object(fields))
+}
+
+fn arguments_definition_to_ts(arguments: &ArgumentsDefinition) -> TSType {
+    TSType::object(arguments.input_values.iter().map(|argument| {
+        (
+            ObjectKey::from(&argument.name),
+            get_ts_type_of_type(&argument.r#type, |name| {
+                TSType::TypeVariable((&name.name).into())
+            }),
+            argument.description.as_ref().map(|s| s.to_string()),
+        )
+    }))
+    .into_readonly()
 }
