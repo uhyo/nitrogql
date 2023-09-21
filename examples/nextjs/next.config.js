d818327f351f7291e980464e97ed775479775456
uhyo <uhyo@uhy.ooo>
2023-09-21 22:45:22 +0900

    chore: deps

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.graphql$/,
