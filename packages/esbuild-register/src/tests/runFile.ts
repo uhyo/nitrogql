ebfee1cd940b28da6b395025e39713c3412f6d7b
uhyo <uhyo@uhy.ooo>
2023-11-04 17:27:48 +0900

    support testing in 20.5 (failing)

--- packages/esbuild-register/src/tests/runFile.ts
+++ packages/esbuild-register/src/tests/runFile.ts
@@ -2,12 +2,28 @@ import { promisify } from "node:util";
 import { execFile } from "node:child_process";
 
 const register = new URL("../../dist/index.js", import.meta.url);
+const hook = new URL("../../dist/hook.js", import.meta.url);
+const nodeVersion = process.versions.node.split(".").map((x) => Number(x)) as [
+  number,
+  number,
+  number
+];
 
 export async function runNode(path: string): Promise<string> {
-  const { stdout } = await promisify(execFile)(
-    process.execPath,
-    ["--import", register.toString(), path],
-    {}
-  );
-  return stdout;
+  // >= Node 20.6.0
+  if (nodeVersion[0] > 20 || (nodeVersion[0] === 20 && nodeVersion[1] >= 6)) {
+    const { stdout } = await promisify(execFile)(
+      process.execPath,
+      ["--loader", register.toString(), path],
+      {}
+    );
+    return stdout;
+  } else {
+    const { stdout } = await promisify(execFile)(
+      process.execPath,
+      ["--experimental-loader", hook.toString(), path],
+      {}
+    );
+    return stdout;
+  }
 }
