bbce60c5e1498d805d04acb769f3457be662513b
uhyo <uhyo@uhy.ooo>
2023-11-16 21:51:20 +0900

    fix tests

--- packages/esbuild-register/src/tests/runFile.ts
+++ packages/esbuild-register/src/tests/runFile.ts
@@ -2,7 +2,8 @@ import { promisify } from "node:util";
 import { execFile } from "node:child_process";
 import { pathToFileURL } from "node:url";
 
-const register = new URL("../../dist/index.js", pathToFileURL(__filename));
+const registerMJS = new URL("../../dist/index.mjs", pathToFileURL(__filename));
+const registerCJS = new URL("../../dist/index.cjs", pathToFileURL(__filename));
 const hook = new URL("../../dist/hook.mjs", pathToFileURL(__filename));
 const nodeVersion = process.versions.node.split(".").map((x) => Number(x)) as [
   number,
@@ -15,7 +16,7 @@ export async function runNode(path: string): Promise<string> {
   if (nodeVersion[0] > 20 || (nodeVersion[0] === 20 && nodeVersion[1] >= 6)) {
     const { stdout } = await promisify(execFile)(
       process.execPath,
-      ["--import", register.toString(), path],
+      ["--import", registerMJS.toString(), path],
       {}
     );
     return stdout;
@@ -24,7 +25,7 @@ export async function runNode(path: string): Promise<string> {
       process.execPath,
       [
         "--require",
-        register.toString(),
+        registerCJS.toString(),
         "--experimental-loader",
         hook.toString(),
         path,
