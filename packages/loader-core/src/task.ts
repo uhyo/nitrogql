7a8da6f5e00075eb4f04f2c5e3d7bdf8f9a94e0c
uhyo <uhyo@babel.jp>
2023-12-07 21:51:31 +0900

    implement loader-core

---
+++ packages/loader-core/src/task.ts
@@ -0,0 +1,90 @@
+import { WasmError, type WASMBin } from "./bin.js";
+
+export type TaskStatus =
+  | {
+      status: "fileRequired";
+      /**
+       * The file that is required.
+       */
+      files: readonly string[];
+    }
+  | {
+      status: "ready";
+    };
+
+/**
+ * Represents a task of converting one GraphQL file to JavaScript.
+ */
+export class Task {
+  #bin: WASMBin;
+  readonly taskId: number;
+  constructor(bin: WASMBin, taskId: number) {
+    this.#bin = bin;
+    this.taskId = taskId;
+  }
+
+  /**
+   * Returns the current status of the task.
+   */
+  status(): TaskStatus {
+    const getRequiredFilesResult = this.#bin.exports.get_required_files(
+      this.taskId
+    );
+    if (!getRequiredFilesResult) {
+      throw new WasmError(
+        "graphql-loader failed to get required files",
+        this.#bin
+      );
+    }
+    const requiredFiles = this.#bin.readResult().split("\n").filter(Boolean);
+    if (requiredFiles.length === 0) {
+      return { status: "ready" };
+    }
+    return { status: "fileRequired", files: requiredFiles };
+  }
+
+  /**
+   * Supply one file to the task.
+   */
+  supplyFile(filePath: string, source: string) {
+    const filePathString = this.#bin.alloc.allocString(filePath);
+    const sourceString = this.#bin.alloc.allocString(source);
+    try {
+      const result = this.#bin.exports.load_file(
+        this.taskId,
+        filePathString.ptr,
+        filePathString.size,
+        sourceString.ptr,
+        sourceString.size
+      );
+      if (!result) {
+        throw new WasmError("graphql-loader failed to load file", this.#bin);
+      }
+    } finally {
+      filePathString.free();
+      sourceString.free();
+    }
+  }
+
+  /**
+   * Emit JavaScript.
+   */
+  emit() {
+    const result = this.#bin.exports.emit_js(this.taskId);
+    if (!result) {
+      throw new WasmError(
+        "graphql-loader failed to emit JavaScript",
+        this.#bin
+      );
+    }
+    const js = this.#bin.readResult();
+    return js;
+  }
+
+  /**
+   * Free the task.
+   */
+  free() {
+    this.#bin.exports.free_task(this.taskId);
+  }
+}
---
+++ packages/loader-core/src/wasm.ts
@@ -0,0 +1,32 @@
+/**
+ * API exposed by crates/graphql-loader.
+ */
+export type WASMInterface = {
+  memory: WebAssembly.Memory;
+  init(debug: number): void;
+  alloc_string(size: number): number;
+  free_string(ptr: number, size: number): void;
+  get_result_ptr(): number;
+  get_result_size(): number;
+  initiate_task(
+    filename_ptr: number,
+    filename_size: number,
+    input_ptr: number,
+    input_size: number
+  ): number;
+  get_required_files(task_id: number): number;
+  load_file(
+    task_id: number,
+    filename_ptr: number,
+    filename_size: number,
+    input_ptr: number,
+    input_size: number
+  ): number;
+  emit_js(task_id: number): number;
+  free_task(task_id: number): void;
+  get_log(): void;
+};
+
+export type WASMInstance = WebAssembly.Instance & {
+  exports: WASMInterface;
+};
