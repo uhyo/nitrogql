1ea91737eb93541f6e55c07399e6fe9160c95b9e
uhyo <uhyo@uhy.ooo>
2024-02-01 22:19:50 +0900

    perf: add node_module optimization to monkey patched resolveFileName

--- packages/esbuild-register/src/register.ts
+++ packages/esbuild-register/src/register.ts
@@ -4,10 +4,19 @@ import { resolveModuleSync } from "./core.js";
 import { fileURLToPath, pathToFileURL } from "node:url";
 import { cjsHook } from "./legacy.js";
 
+const isNodeModules = (url: string) => {
+  return url.includes("/node_modules/");
+};
+
 /**
  * Registers the esbuild hook.
  */
 export function register() {
+  /**
+   * Whether to include node_modules to conversion target.
+   */
+  const includeNodeModules: boolean = !!process.env.INCLUDE_NODE_MODULES;
+
   // ESM loader (>= Node 20.6.0 or >= 18.19.0)
   // @ts-expect-error
   if (nodeModule.register) {
@@ -27,10 +36,16 @@ export function register() {
       specifier: string,
       parent: NodeJS.Module | undefined
     ) => {
-      const resolved = resolveModuleSync(
-        specifier,
-        parent && pathToFileURL(parent.filename).toString()
-      );
+      const parentURL = parent && pathToFileURL(parent.filename).toString();
+      if (
+        !includeNodeModules &&
+        parentURL !== undefined &&
+        isNodeModules(parentURL)
+      ) {
+        return originalResolveFilename(specifier, parent);
+      }
+
+      const resolved = resolveModuleSync(specifier, parentURL);
       if (resolved !== undefined) {
         return fileURLToPath(resolved);
       }
