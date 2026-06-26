abfdf627b1b5f41f9d7460f4d3c61b32a59888ba
uhyo <uhyo@uhy.ooo>
2026-06-26 12:36:02 +0900

    Convert @nitrogql/esbuild-register to ESM and upgrade vitest to 4 (#70)

--- packages/esbuild-register/src/index.cts
+++
@@ -1,3 +0,0 @@
-import { register } from "./register.js";
-
-register();
--- packages/esbuild-register/src/tests/runFile.ts
+++ packages/esbuild-register/src/tests/runFile.ts
@@ -1,20 +1,7 @@
-import path from "node:path";
 import { promisify } from "node:util";
 import { execFile } from "node:child_process";
-import { pathToFileURL } from "node:url";
 
-const registerMJS = new URL("../../dist/index.mjs", pathToFileURL(__filename));
-const registerCJS = path.join(__dirname, "../../dist/index.cjs");
-const hook = new URL("../../dist/hook.mjs", pathToFileURL(__filename));
-const nodeVersion = process.versions.node.split(".").map((x) => Number(x)) as [
-  number,
-  number,
-  number,
-];
-const nodeSupportsModuleRegisterAPI =
-  nodeVersion[0] > 20 ||
-  (nodeVersion[0] === 20 && nodeVersion[1] >= 6) ||
-  (nodeVersion[0] === 18 && nodeVersion[1] >= 19);
+const registerMJS = new URL("../../dist/index.mjs", import.meta.url);
 
 export async function runNode(
   path: string,
@@ -26,29 +13,12 @@ export async function runNode(
     options?.dataUrlResolutionBase !== undefined
       ? { DATA_URL_RESOLUTION_BASE: options.dataUrlResolutionBase }
       : undefined;
-  if (nodeSupportsModuleRegisterAPI) {
-    const { stdout } = await promisify(execFile)(
-      process.execPath,
-      ["--import", registerMJS.toString(), path],
-      {
-        env,
-      },
-    );
-    return stdout;
-  } else {
-    const { stdout } = await promisify(execFile)(
-      process.execPath,
-      [
-        "--require",
-        registerCJS.toString(),
-        "--experimental-loader",
-        hook.toString(),
-        path,
-      ],
-      {
-        env,
-      },
-    );
-    return stdout;
-  }
+  const { stdout } = await promisify(execFile)(
+    process.execPath,
+    ["--import", registerMJS.toString(), path],
+    {
+      env,
+    },
+  );
+  return stdout;
 }
